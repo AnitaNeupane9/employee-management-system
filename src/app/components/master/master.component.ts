d877377aa6d3483b07443fad73e1d9037f24bc50
AnitaNeupane9 <anitaneupane2062@gmail.com>
2024-12-03 14:53:05 +0545

    Data Binding and Directives

---
+++ src/app/components/master/master.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import {RolesComponent} from '../roles/roles.component';
+import {DesignationComponent} from '../designation/designation.component';
+import {NgClass, NgIf} from '@angular/common';
+
+@Component({
+  selector: 'app-master',
+  imports: [
+    RolesComponent,
+    DesignationComponent,
+    NgIf,
+    NgClass
+  ],
+  templateUrl: './master.component.html',
+  styleUrl: './master.component.css'
+})
+export class MasterComponent {
+  currentComponent: String = "Roles";
+
+  changeTab(tabName: string){
+    this.currentComponent = tabName;
+
+  }
+}
---
+++ src/app/components/roles/roles.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import {FormsModule} from '@angular/forms';
+
+@Component({
+  selector: 'app-roles',
+  imports: [FormsModule],
+  templateUrl: './roles.component.html',
+  styleUrls: ['./roles.component.css']
+})
+export class RolesComponent {
+// // string, number, boolean, date, object, array, null, undefined
+//   firstname: string = "Angular";
+//   angularVersion = "Version 19";
+//   version: number = 18;
+//   inActive: boolean = false;
+//   currentDate: Date = new Date();
+//   inputType: string = "radio";
+//   selectedState: string = '';
+//
+//
+//   // Creating function
+//   showWelcomeAlert(){
+//     alert("Welcome Aliens!")
+//   }
+//
+//   // Parameterized function
+//   showMessage(message: string){
+//     alert(message)
+//   }
+  
+}
